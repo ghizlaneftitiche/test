d792c285656d77b4ade0175af69bdff6db79e23d
Ibtissam_Fakir <iibtissamfakr@gmail.com>
2026-04-21 21:43:35 +0100

    création de AdminSeeder pour l'initialisation du compte admin

---
+++ BackendCentre-AlFadl/database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nomComplet' => 'Administrateur Principal',
+            'email_genere' => 'admin@centre-alfadl.ma',
+            'email_personnel' => 'votre-email@gmail.com',
+            'password' => Hash::make('admin123'),
+            'numTel' => '0600000000',
+            'role' => 'admin',
+            'formation_id' => null,
+        ]);
+    }
+}
