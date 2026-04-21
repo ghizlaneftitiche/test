6318f7155b25284ed8fc36520fedddb2493bb332
Ibtissam_Fakir <iibtissamfakr@gmail.com>
2026-04-21 21:38:03 +0100

    Ajout des API de login, logout et création de formateurs

--- BackendCentre-AlFadl/routes/api.php
+++ BackendCentre-AlFadl/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::post('/admin/create-formateur', [\App\Http\Controllers\AdminController::class, 'store']);
+});
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [\App\Http\Controllers\LoginController::class, 'logout']);
 });
