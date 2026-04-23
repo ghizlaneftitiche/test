10744e1d6b342b9c86f045e55daddec4ee4be68c
Ibtissam_Fakir <iibtissamfakr@gmail.com>
2026-04-23 18:58:53 +0100

    Ajout de deux routes pour récupérer les branches et les modules de la base de données

--- BackendCentre-AlFadl/routes/api.php
+++ BackendCentre-AlFadl/routes/api.php
@@ -1,23 +1,17 @@
 <?php
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RecupererDonnees;
+use App\Http\Controllers\LoginController;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+
+
+
+Route::post('/login', [LoginController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::post('/admin/create-formateur', [\App\Http\Controllers\AdminController::class, 'store']);
-});
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [\App\Http\Controllers\LoginController::class, 'logout']);
+Route::post('/admin/create-formateur', [AdminController::class, 'store']);
+Route::get('/admin/get-branches', [RecupererDonnees::class, 'getBranches']);
+Route::get('/admin/get-modules', [RecupererDonnees::class, 'getModules']);
+Route::post('/logout', [LoginController::class, 'logout']);
 });
