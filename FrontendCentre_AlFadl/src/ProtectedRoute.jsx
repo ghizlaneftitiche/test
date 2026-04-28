bd089142b398ed1cc5aacdef6eda926c006e930b
Ibtissam_Fakir <iibtissamfakr@gmail.com>
2026-04-28 12:19:12 +0100

    Sécurisation des routes

---
+++ FrontendCentre_AlFadl/src/ProtectedRoute.jsx
@@ -0,0 +1,18 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children, allowedRoles }) => {
+  const token = localStorage.getItem("token");
+  const userRole = localStorage.getItem("user_role");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(userRole)) {
+    return <Navigate to="/unauthorized" replace />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
