4bdf70bbfa3d2d5e662081a8ae1d4e6164fecaef
Ibtissam_Fakir <iibtissamfakr@gmail.com>
2026-04-28 12:17:03 +0100

    Sécurisation des routes

--- FrontendCentre_AlFadl/src/App.jsx
+++ FrontendCentre_AlFadl/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import SidBar from "./Layout/SidBar";
 import Dashboard from "./adminComponents/Dashboard";
 import FormateurModule from "./adminComponents/FormateurModule";
@@ -7,11 +7,14 @@ import FormateurBranche from "./adminComponents/FormateurBranche";
 import AjouterFormateur from "./adminComponents/AjouterFormateur";
 import Login from "./adminComponents/Login";
 import ListeFormateurs from "./adminComponents/ListeFormateurs";
-
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   const location = useLocation();
   const isLoginPage = location.pathname === "/";
+  
+  // On récupère le rôle pour savoir si on affiche la sidebar
+  const userRole = localStorage.getItem("user_role");
 
   return (
     <>
@@ -21,14 +24,53 @@ function App() {
         </Routes>
       ) : (
         <div className="flex">
-          <SidBar />
+          {userRole === "admin" && <SidBar />}
+          
           <div className="flex-1 bg-white p-6">
             <Routes>
-              <Route path="/admin-dashboard" element={<Dashboard />} />
-              <Route path="/formateurModule" element={<FormateurModule />} />
-              <Route path="/formateurBranche" element={<FormateurBranche />} />
-              <Route path="/ajouter-formateur" element={<AjouterFormateur />} />
-              <Route path="/liste-formateurs" element={<ListeFormateurs />} />
+              <Route 
+                path="/admin-dashboard" 
+                element={
+                  <ProtectedRoute allowedRoles={["admin"]}>
+                    <Dashboard />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/ajouter-formateur" 
+                element={
+                  <ProtectedRoute allowedRoles={["admin"]}>
+                    <AjouterFormateur />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/liste-formateurs" 
+                element={
+                  <ProtectedRoute allowedRoles={["admin"]}>
+                    <ListeFormateurs />
+                  </ProtectedRoute>
+                } 
+              />
+
+              <Route 
+                path="/FormateurModule" 
+                element={
+                  <ProtectedRoute allowedRoles={["FormateurModule"]}>
+                    <FormateurModule />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/FormateurBranche" 
+                element={
+                  <ProtectedRoute allowedRoles={["FormateurBranche"]}>
+                    <FormateurBranche />
+                  </ProtectedRoute>
+                } 
+              />
+
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </div>
         </div>
