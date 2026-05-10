7b145e52d57ffd8fe8e2ecfd06025ca258815cb1
ghizlane ftitiche <ghizlaneftitiche6@gmail.com>
2026-05-10 18:17:31 +0100

    Utilisation d'une variable d'environnement pour l'API

--- FrontendCentre_AlFadl/src/api.jsx
+++ FrontendCentre_AlFadl/src/api.jsx
@@ -1,1 +1,4 @@
-export const api='http://127.0.0.1:8000'
+// export const api='http://127.0.0.1:8000'
+
+// Il cherche la variable VITE_API_URL, sinon il utilise localhost par défaut
+export const api = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
